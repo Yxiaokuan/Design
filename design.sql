25bcb09cd693c7b4b4e64fb331e7bd76d742d516
loneyclown <loneyclown@outlook.com>
2018-12-25 23:50:11 +0800

    Update design.sql

--- design.sql
+++ design.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50527
 File Encoding         : 65001
 
-Date: 2018-12-25 22:35:39
+Date: 2018-12-25 23:49:25
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,15 +27,6 @@ CREATE TABLE `class` (
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of class
--- ----------------------------
-INSERT INTO `class` VALUES ('1', '移应S2016-1', '1');
-INSERT INTO `class` VALUES ('2', '移应S2016-2', '1');
-INSERT INTO `class` VALUES ('3', '移应S2016-3', '1');
-INSERT INTO `class` VALUES ('4', '模具S2016-1', '3');
-INSERT INTO `class` VALUES ('5', '模具S2016-2', '3');
-
--- ----------------------------
 -- Table structure for college
 -- ----------------------------
 DROP TABLE IF EXISTS `college`;
@@ -44,14 +35,6 @@ CREATE TABLE `college` (
   `collegeName` varchar(50) NOT NULL COMMENT '院系名',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of college
--- ----------------------------
-INSERT INTO `college` VALUES ('1', '信息系');
-INSERT INTO `college` VALUES ('2', '机械系');
-INSERT INTO `college` VALUES ('3', '汽车系');
-INSERT INTO `college` VALUES ('4', '电气系');
 
 -- ----------------------------
 -- Table structure for designtopic
@@ -70,28 +53,6 @@ CREATE TABLE `designtopic` (
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of designtopic
--- ----------------------------
-INSERT INTO `designtopic` VALUES ('1', '基于B/S的图书管理系统', '基于B/S的图书管理系统的要求。', '1', '2', '2', null, null);
-INSERT INTO `designtopic` VALUES ('2', '基于SSM的网上商城系统', '基于SSM的网上商城系统的要求。', '2', '1', '2', null, null);
-INSERT INTO `designtopic` VALUES ('3', '基于SSH的驾校管理系统', '基于SSM的网上商城系统的要求。', '1', '2', '2', '', 'archivetemp1基于SSM的毕业设计选题系统设计与实现 课题任务书 .doc');
-INSERT INTO `designtopic` VALUES ('4', '基于A/S的网上书店系统', '基于A/S的网上书店系统的要求。', '2', '2', '2', null, null);
-INSERT INTO `designtopic` VALUES ('5', '基于B/S的图书管理系统', '是一个要求。', '4', '2', '2', null, null);
-INSERT INTO `designtopic` VALUES ('6', '基于B/S的图书管理系统', '是一个要求。', '2', '2', '2', null, null);
-INSERT INTO `designtopic` VALUES ('7', '基于B/S的图书管理系统', '是一个要求。', '5', '2', '2', null, null);
-INSERT INTO `designtopic` VALUES ('8', '基于B/S的图书管理系统', '是一个要求。', '1', '2', '2', null, null);
-INSERT INTO `designtopic` VALUES ('9', '基于B/S的图书管理系统', '是一个要求。', '1', '2', '2', null, null);
-INSERT INTO `designtopic` VALUES ('10', '基于B/S的图书管理系统', '是一个要求。', '2', '2', '3', null, null);
-INSERT INTO `designtopic` VALUES ('11', '基于B/S的图书管理系统', '是一个要求。', '1', '2', '3', null, null);
-INSERT INTO `designtopic` VALUES ('12', '基于B/S的图书管理系统', '是一个要求。', '1', '2', '0', null, null);
-INSERT INTO `designtopic` VALUES ('13', '基于B/S的图书管理系统', '是一个要求。', '2', '2', '0', null, null);
-INSERT INTO `designtopic` VALUES ('14', '基于B/S的图书管理系统', '是一个要求。', '1', '2', '0', null, null);
-INSERT INTO `designtopic` VALUES ('15', '课题名称1', '课题要求1', '2', null, '0', null, 'archivetemp1基于SSM的毕业设计选题系统设计与实现 课题任务书 .doc');
-INSERT INTO `designtopic` VALUES ('16', '课题名称2', '课题要求2', '2', '2', '0', null, 'archivetemp1基于SSM的毕业设计选题系统设计与实现 课题任务书 .doc');
-INSERT INTO `designtopic` VALUES ('17', '的穆斯林大家放慢了速度', '是多么快乐是麻烦了', '2', null, '0', null, 'archivetemp1基于SSM的毕业设计选题系统设计与实现 课题任务书 .doc');
-INSERT INTO `designtopic` VALUES ('18', '上的梵蒂冈梵蒂冈点歌，了', '的身份来到调动， ；地方过分的事，发历史记录色放而是', '2', null, '0', null, 'archivetemp1基于SSM的毕业设计选题系统设计与实现 课题任务书 .doc');
-
--- ----------------------------
 -- Table structure for expertise
 -- ----------------------------
 DROP TABLE IF EXISTS `expertise`;
@@ -100,14 +61,6 @@ CREATE TABLE `expertise` (
   `expertiseName` varchar(50) NOT NULL COMMENT '教师专长名字',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of expertise
--- ----------------------------
-INSERT INTO `expertise` VALUES ('1', '软件技术');
-INSERT INTO `expertise` VALUES ('2', '移动应用');
-INSERT INTO `expertise` VALUES ('3', '计算机网络');
-INSERT INTO `expertise` VALUES ('4', '数字媒体');
 
 -- ----------------------------
 -- Table structure for role
@@ -122,12 +75,6 @@ CREATE TABLE `role` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of role
--- ----------------------------
-INSERT INTO `role` VALUES ('1', 'clown', '123', '0');
-INSERT INTO `role` VALUES ('2', '张三', '123', '1');
-
--- ----------------------------
 -- Table structure for student
 -- ----------------------------
 DROP TABLE IF EXISTS `student`;
@@ -140,18 +87,7 @@ CREATE TABLE `student` (
   `password` varchar(50) NOT NULL COMMENT '系统登录密码',
   `topicId` int(4) DEFAULT NULL COMMENT '毕业设计课题ID',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of student
--- ----------------------------
-INSERT INTO `student` VALUES ('1', '李寻欢', '移应S2016-1-1', '1', '1', '1234', '3');
-INSERT INTO `student` VALUES ('2', '学生2号', '移应S2016-1-2', '1', '1', '123', '2');
-INSERT INTO `student` VALUES ('3', '学生3号', '移应S2016-1-3', '1', '1', '123', '3');
-INSERT INTO `student` VALUES ('4', '李琪琪', '移应S2016-2-1', '1', '2', '123', '4');
-INSERT INTO `student` VALUES ('5', '江永', '模具S2016-1-1', '3', '4', '123', '5');
-INSERT INTO `student` VALUES ('6', '李逵', '模具S2016-1-2', '3', '4', '123', '1');
-INSERT INTO `student` VALUES ('8', '学生4号', '移应S2016-1-4', '1', '1', '123', '1');
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for teacher
@@ -168,12 +104,4 @@ CREATE TABLE `teacher` (
   KEY `teacher_expertise_id` (`expertiseId`),
   CONSTRAINT `teacher_college_id` FOREIGN KEY (`collegeId`) REFERENCES `college` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `teacher_expertise_id` FOREIGN KEY (`expertiseId`) REFERENCES `expertise` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of teacher
--- ----------------------------
-INSERT INTO `teacher` VALUES ('1', '聂时时', '1', '1', '123');
-INSERT INTO `teacher` VALUES ('2', '葛木宗一郎', '1', '1', '321');
-INSERT INTO `teacher` VALUES ('4', '王二狗', '4', '4', '123');
-INSERT INTO `teacher` VALUES ('5', '大大怪', '1', '2', '123');
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
